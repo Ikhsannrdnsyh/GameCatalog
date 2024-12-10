39bad6138163cddba3f5100118c254c5e2186e46
Ikhsannrdnsyh <ikhsannurdiansyah78@gmail.com>
2024-12-10 21:34:50 +0700

    Initial Commit

---
+++ GameCatalogTests/GameCatalogTests.swift
@@ -0,0 +1,17 @@
+//
+//  GameCatalogTests.swift
+//  GameCatalogTests
+//
+//  Created by Mochamad Ikhsan Nurdiansyah on 10/12/24.
+//
+
+import Testing
+@testable import GameCatalog
+
+struct GameCatalogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
